2de295df87a5bb376c325f3089d9b1c5c86757f0
George Bentes <george.bentes@gmail.com>
2016-08-01 21:44:17 -0400

    Route to authenticate and authorizate

---
+++ routes/authRoutes.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var authRouter = express.Router();
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/appLoginDB');
+var passport = require('passport');
+
+/*
+authRouter.use(function(req, res, next){
+	if(!req.user){
+		res.redirect('/');
+	}
+	next();
+});
+*/
+
+authRouter.post('/signUp', function(req, res){
+	console.log(req.body);
+
+	var collection = db.collection('users');
+	var user = {
+		username: req.body.username,
+		password:req.body.password
+	};
+
+	collection.insert(user, function(err, results){
+		req.login(results, function(){
+			res.redirect('/auth/profile');
+		});
+	});
+});
+
+authRouter.post('/signIn', passport.authenticate('local', {failureRedirect: '/'}), function(req, res){
+	res.redirect('/auth/profile');
+});
+
+authRouter.route('/profile').all(function(req, res, next){
+	if(!req.user){
+		res.redirect('/');
+	}
+	next();
+}).get(function(req, res){
+	res.json(req.user);
+});
+
+module.exports = authRouter;
