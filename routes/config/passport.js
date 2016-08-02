d84367cc9357bd1ad7c0bfa8dabc1c230d4ceeb6
George Bentes <george.bentes@gmail.com>
2016-08-01 21:44:33 -0400

    File that puts the user in a session

---
+++ routes/config/passport.js
@@ -0,0 +1,19 @@
+var passport = require('passport');
+
+module.exports = function (app) {
+	app.use(passport.initialize());
+	app.use(passport.session());
+
+	// Bundle the user up, store in the session for later
+	passport.serializeUser(function(user, done){
+		done(null, user);
+	});
+
+	// Put the user back out of the session.
+	passport.deserializeUser(function(user, done){
+		done(null, user);
+	});
+
+	// This file belows, deal with the database stuff...
+	require('./strategies/local.strategy')();
+}
