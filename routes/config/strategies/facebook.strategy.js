0bb11aa73c07cf4c0fab0f874d54e8466f5a7e92
George Bentes <george.bentes@gmail.com>
2016-08-02 23:18:22 -0400

    Pushin the facebook's passport strategy

---
+++ routes/config/strategies/facebook.strategy.js
@@ -0,0 +1,32 @@
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/appLoginDB');
+
+module.exports = function () {
+	passport.use(new FacebookStrategy({
+		clientID: '1441798922500688',
+		clientSecret: FACEBOOK_APP_SECRET,
+		callbackURL: "localhost:3000/auth/facebook/callback"
+	}, function(accessToken, refreshToken, profile, done){
+		var user = {
+			username: profile.displayName,
+			email: profile.emails[0].value,
+			photo: profile.photos[0].value,
+			provider: profile.provider
+		}
+
+		var collection = db.collection('users');
+
+		collection.findOne({email:user.email}, function(err, result){
+			if(result){
+				done(null, user);
+			}else{
+				collection.insert(user, function(err, result){
+					done(null, result);
+				});
+			}
+		});
+	}));
+}
