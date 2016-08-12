73c0a12a88817b3e7b7b4f7010df46fc1e2a09fd
George Bentes <george.bentes@gmail.com>
2016-08-12 14:55:49 -0400

    Getting and sending the Authentication error message to the user when it fails using the flash technology

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Login Module' });
+router.get('/', function (req, res, next) {
+	var errorMessage = req.flash('error');
+	res.render('index', {
+		title: 'Login Module',
+		errorLoginMessage: errorMessage
+	});
 });
 
 module.exports = router;
