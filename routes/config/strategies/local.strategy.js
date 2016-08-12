630a1059c8f4ee6a1a220fdc8e785f6fa45bb3b7
George Bentes <george.bentes@gmail.com>
2016-08-12 14:54:16 -0400

    - Removed the Mongo and Monk instances to add the User created through Mongoose
    - Changed the LocalStrategies default params
    - Created a function to handle an error if data is not valid

--- routes/config/strategies/local.strategy.js
+++ routes/config/strategies/local.strategy.js
@@ -1,23 +1,33 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/appLoginDB');
+var User = require('../../../models/user');
 
 module.exports = function () {
-	passport.use(new LocalStrategy({
-		usernameField: 'username',
-		passwordField: 'password'
-	},function(username, password, done){
 
-		var collection = db.collection('users');
-		collection.findOne({username:username}, function(err, result){
-			if(result.password === password){
-				var user = result;
-				done(null, user);
-			}else{
-				done(null, false, {message: 'Bad password'});
+	passport.use(new LocalStrategy({
+		usernameField: 'email',
+		passwordField: 'password'
+	}, function (email, password, done) {
+		User.findOne({email: email}, function (err, user) {
+			if (err) {
+				console.error(err);
+				done(err);
+			}
+
+			if (user) {
+				if (user.password === password) {
+					done(null, user)
+				}else{
+					userPasswordFailed('Incorrect password', done);
+				}
+			} else{
+				userPasswordFailed('User not found', done);
 			}
 		});
 	}));
+
+	var userPasswordFailed = function (realProblem, done) {
+		console.error(realProblem);
+		done(null, false, {message: 'User or password not valid!'});
+	}
 }
