c67c0ff6b391e3f261207e753f65191e7160837b
George Bentes <george.bentes@gmail.com>
2016-08-12 14:48:17 -0400

    Created the User model through Mongoose technology

---
+++ models/user.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var userSchema = mongoose.Schema({
+	name:String,
+	email:String,
+	password:String,
+	provider:String,
+	providerID:String,
+	photo:String
+});
+
+
+module.exports = mongoose.model("User", userSchema);
