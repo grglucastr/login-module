456f214718d6ecdcc3735730c88f8afbf3285476
George Bentes <george.bentes@gmail.com>
2016-08-12 23:51:58 -0400

    Added the flash error message when sign up fails

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,12 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
 	var errorMessage = req.flash('error');
+	var errorSignUpMessage = req.flash('errorSignUpMessage');
+
 	res.render('index', {
 		title: 'Login Module',
-		errorLoginMessage: errorMessage
+		errorLoginMessage: errorMessage,
+		errorSignUpMessage: errorSignUpMessage
 	});
 });
 
