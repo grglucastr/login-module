dd03f7bc59fc576124b18a27e951b6714a2a27c6
George Bentes <george.bentes@gmail.com>
2016-08-12 23:50:37 -0400

    Creating and saving the user. Needs to test this feature

--- routes/config/strategies/facebook.strategy.js
+++ routes/config/strategies/facebook.strategy.js
@@ -1,31 +1,29 @@
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/appLoginDB');
+var User = require('../../../models/user');
 
 module.exports = function () {
 	passport.use(new FacebookStrategy({
 		clientID: '1441798922500688',
-		clientSecret: 'FACEBOOK_APP_SECRET',
-		callbackURL: "localhost:3000/auth/facebook/callback"
-	}, function(accessToken, refreshToken, profile, done){
-		var user = {
-			username: profile.displayName,
+		clientSecret: '3956a34fc9dd813efba57421ae068072',
+		callbackURL: 'http://localhost:3000/auth/facebook/callback',
+		profileFields: ['id', 'displayName', 'email', 'profileUrl', 'photos']
+	}, function (accessToken, refreshToken, profile, done) {
+
+		var newUser = new User({
+			name: profile.displayName,
 			email: profile.emails[0].value,
-			photo: profile.photos[0].value,
-			provider: profile.provider
-		}
+			provider: profile.provider,
+			providerID: profile.id,
+			photo: profile.photos[0].value
+		});
 
-		var collection = db.collection('users');
-
-		collection.findOne({email:user.email}, function(err, result){
-			if(result){
+		newUser.save(function (err, user) {
+			if (err) {
+				console.error(`\n\n Error on save the user through Facebook: \n ${err} \n\n`);
+				done(err);
+			} else {
 				done(null, user);
-			}else{
-				collection.insert(user, function(err, result){
-					done(null, result);
-				});
 			}
 		});
 	}));
